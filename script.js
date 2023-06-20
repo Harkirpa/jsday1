74c6a3e2a2c6951f797680f102e524bc47273d64
Harkirpa <kirpa704@gmail.com>
2023-06-20 11:53:13 +0530

    day1

---
+++ script.js
@@ -0,0 +1,35 @@
+// answer1//
+let a =10;
+let b=10;
+console.log(a==b); // true
+console.log(a===b); // false
+
+// answer2//
+// var example
+var x = 10;
+if (true) {
+  var x = 20;
+}
+console.log(x); // outputs 20​
+// let example
+let y = 10;
+if (true) {
+  let y = 20;
+}
+console.log(y); // outputs 10
+
+// const example
+const z = 10;
+// z = 20; // throws an error
+console.log(z); // outputs 10​
+// answer3//
+console.log(ab);
+let ab= 10;
+abc();
+function abc()
+{
+    console.log("Section ....");
+}
+
+ 
+
